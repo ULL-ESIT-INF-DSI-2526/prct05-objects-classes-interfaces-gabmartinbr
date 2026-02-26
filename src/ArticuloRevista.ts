7103d93449e841c3ec05f3028afa1b3accb62600
gabmartinbr <alu0101539157@ull.edu.es>
2026-02-26 11:20:57 +0000

    ieee revista done

--- src/ArticuloRevista.ts
+++ src/ArticuloRevista.ts
@@ -36,13 +36,44 @@ export class ArticuloRevista extends ElementoBibliografico {
    * - vol. seguido del numero de volumen
    * - no. seguido del numero de la edicion
    * - pp. seguida del rango de paginas separadas por guiones (pp. 12-15)
-   * - usar obj Date, extraer el mes abrev con 3 letras (Jan o ene) y el año completo
+   * - usar obj Date, extraer el mes abrev con 3 letras con punto final (Jan o ene) y el año completo
    * - unir todo lo anterior con , y espacio, termina en . final
    * 
    * Ejemplo de IEEE revista: 
    * J. Pérez y M. García, "Avances en el procesamiento de lenguaje natural," Revista de Inteligencia Artificial Aplicada, vol. 12, no. 3, pp. 45-52, Mar. 2024.
    */
   toIEEE(): string {
-    return 
+    let result: string = ""
+    // 1. [autor1, autor2] => autor1, autor2
+    let autoresClean: string = this.autores.join(", ")
+    result += autoresClean + ", "
+
+    // 2. "titulo articulo,"
+    let tituloClean: string = `"${this.titulo},"`
+    result += tituloClean + " "
+
+    // 3. nombre de la revista
+    let nombreRevistaClean: string = this.nombreRevista
+    result += nombreRevistaClean + ", "
+
+    // 4. volumen
+    let volClean: string = `vol. ${this.volumen}`
+    result += volClean + ", "
+
+    // 5. numero edicion
+    let numeroClean: string = `no. ${this.nRevista}`
+    result += numeroClean + ", "
+
+    // 6. paginas
+    let paginasClean: string = `pp. ${this.nPaginas}`
+    result += paginasClean + ", "
+
+    // 7. fecha
+    let month = this.fechaPublicacion.toLocaleString('en-EN', { month: 'short' })
+    let year = this.fechaPublicacion.getFullYear()
+    let fechaClean: string = `${month}. ${year}`
+    result += fechaClean + "."
+
+    return result
   }
 }
